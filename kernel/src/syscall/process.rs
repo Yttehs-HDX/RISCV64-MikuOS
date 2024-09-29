c891ea36c698e3e7584a97d89bf667420cdc695a
Yttehs <shetty.yttehs@outlook.com>
2024-09-29 21:27:06 +0800

    chore: warn when exit_code != 1

--- kernel/src/syscall/process.rs
+++ kernel/src/syscall/process.rs
@@ -1,7 +1,10 @@
-use log::info;
+use log::{info, warn};
 use crate::batch;
 
 pub fn sys_exit(exit_code: usize) -> ! {
-    info!("Process exited with code {}", exit_code);
+    match exit_code {
+        0 => info!("Process exited with code {}", exit_code),
+        _ => warn!("Process exited with code {}", exit_code),
+    }
     batch::exit_handler();
 }
