00672efe15778fc0220bd689f66b763b6bb934dc
Yttehs <shetty.yttehs@outlook.com>
2024-09-29 21:17:04 +0800

    refactor: sys_exit after user panic

--- kernel/src/trap/mod.rs
+++ kernel/src/trap/mod.rs
@@ -1,7 +1,7 @@
 use core::arch::global_asm;
 use log::{debug, error};
 use riscv::register::{scause::{self, Exception, Trap}, stval, stvec, utvec::TrapMode};
-use crate::{batch, syscall};
+use crate::syscall;
 
 pub use trap_context::*;
 
@@ -22,20 +22,22 @@ fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
             debug!("ecall from U-mode @ {:#x}", cx.sepc);
             cx.sepc += 4;
             syscall::syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]);
-            return cx;
+            cx
         }
         Trap::Exception(Exception::IllegalInstruction) => {
             error!("illegal instruction @ {:#x}, badaddr {:#x}", cx.sepc, stval);
+            syscall::sys_exit(1);
         }
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
             error!("store fault @ {:#x}, badaddr {:#x}", cx.sepc, stval);
+            syscall::sys_exit(1);
         }
         _ => {
             error!("unhandled trap {:?} @ {:#x}", scause.cause(), cx.sepc);
+            syscall::sys_exit(1);
         }
     }
-    batch::exit_handler();
 }
 
 extern "C" {
