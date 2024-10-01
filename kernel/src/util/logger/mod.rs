6d0e392bfd33754536c96c23807ab0958bc61895
Yttehs <shetty.yttehs@outlook.com>
2024-10-01 22:00:42 +0800

    chore: turn off when LOG does not exist

--- kernel/src/util/logger/mod.rs
+++ kernel/src/util/logger/mod.rs
@@ -27,7 +27,7 @@ struct Logger;
 
 impl Log for Logger {
     fn enabled(&self, _metadata: &log::Metadata) -> bool {
-        !matches!(option_env!("LOG"), Some("OFF"))
+        !matches!(option_env!("LOG"), None)
     }
 
     fn log(&self, record: &log::Record) {
