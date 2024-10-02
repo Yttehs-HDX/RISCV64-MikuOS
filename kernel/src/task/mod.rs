65baaf5e365586fc2ea64bda1f9428bfe4139627
Yttehs <shetty.yttehs@outlook.com>
2024-10-02 23:37:46 +0800

    feat: implement switch asm fn

--- kernel/src/task/mod.rs
+++ kernel/src/task/mod.rs
@@ -1,3 +1,11 @@
+use core::arch::global_asm;
+
 pub use context::*;
 
+global_asm!(include_str!("switch.S"));
+
 mod context;
+
+extern "C" {
+    pub fn __switch(current_task_cx: *mut TaskContext, next_task_cx: *const TaskContext);
+}
