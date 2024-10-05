9ea9ab277b68510afa0d29b0a1ecf200ddc98895
Yttehs <shetty.yttehs@outlook.com>
2024-10-05 23:42:59 +0800

    chore: add hint for tests

--- user/src/bin/test_page_fault.rs
+++ user/src/bin/test_page_fault.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+use user_lib::println;
 extern crate user_lib;
 
 #[no_mangle]
 fn main() -> i32 {
+    println!("[User] test_page_fault");
     let ptr = 0xdeadbeef as *mut i32;
     unsafe {
         *ptr = 0;
--- user/src/bin/test_sret.rs
+++ user/src/bin/test_sret.rs
@@ -2,11 +2,13 @@
 #![no_main]
 
 use core::arch::asm;
+use user_lib::println;
 
 extern crate user_lib;
 
 #[no_mangle]
 fn main() -> i32 {
+    println!("[User] test_sret");
     unsafe {
         asm!("sret");
     }
