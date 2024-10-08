c9b86639d9be378ddb4084604116ad4f3a551c8e
Yttehs <shetty.yttehs@outlook.com>
2024-10-08 11:22:11 +0800

    featL implement TimeVal for user

--- user/src/bin/04_test_yield.rs
+++ user/src/bin/04_test_yield.rs
@@ -1,19 +1,19 @@
 #![no_std]
 #![no_main]
 
-use user_lib::{get_time, println, yield_};
+use user_lib::{get_time, println, yield_, TimeVal};
 
 extern crate user_lib;
 
 #[no_mangle]
 fn main() -> i32 {
     println!("[User] test_yield");
-    println!("[User] test_yield: time = {}", get_time());
+    println!("[User] test_yield: time = {}", get_time().format());
     let time = get_time();
-    while get_time() - time < 100_000 {
+    while get_time() - time < TimeVal::new(3, 0) {
         yield_();
     }
-    println!("[User] test_yield: time = {}", get_time());
+    println!("[User] test_yield: time = {}", get_time().format());
     println!("[User] test_yield: done");
     0
 }
---
+++ user/src/timer.rs
@@ -0,0 +1,133 @@
+use core::ops;
+use alloc::string::String;
+use alloc::format;
+
+const MILLIS_PER_SEC: usize = 1_000;
+const MICRO_PER_SEC: usize = 1_000_000;
+
+// region TimeVal begin
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq)]
+pub struct TimeVal {
+    sec: usize,
+    usec: usize,
+}
+
+impl TimeVal {
+    pub fn empty() -> Self {
+        TimeVal { sec: 0, usec: 0 }
+    }
+
+    pub fn new(sec: usize, usec: usize) -> Self {
+        TimeVal { sec, usec }
+    }
+
+    pub fn get_time(&self, unit: TimeUnit) -> usize {
+        match unit {
+            TimeUnit::Hour => self.sec / 3600,
+            TimeUnit::Min => self.sec / 60,
+            TimeUnit::Sec => self.sec,
+            TimeUnit::Msec => self.sec * MILLIS_PER_SEC + self.usec / MICRO_PER_SEC,
+            TimeUnit::Usec => self.sec * MICRO_PER_SEC + self.usec,
+        }
+    }
+
+    pub fn format(&self) -> String {
+        format!(
+            "{:02}:{:02}:{:02}.{:06}",
+            self.sec / 3600,
+            self.sec / 60 % 60,
+            self.sec % 60,
+            self.usec
+        )
+    }
+}
+
+impl ops::Add<TimeVal> for TimeVal {
+    type Output = TimeVal;
+
+    fn add(self, rhs: TimeVal) -> TimeVal {
+        let mut sec = self.sec + rhs.sec;
+        let mut usec = self.usec + rhs.usec;
+        if usec >= MICRO_PER_SEC {
+            sec += 1;
+            usec -= MICRO_PER_SEC;
+        }
+        TimeVal { sec, usec }
+    }
+}
+
+impl ops::Sub<TimeVal> for TimeVal {
+    type Output = TimeVal;
+
+    fn sub(self, rhs: TimeVal) -> TimeVal {
+        let mut sec = self.sec as isize - rhs.sec as isize;
+        let mut usec = self.usec as isize - rhs.usec as isize;
+        if usec < 0 {
+            sec -= 1;
+            usec += MICRO_PER_SEC as isize;
+        }
+        TimeVal {
+            sec: sec as usize,
+            usec: usec as usize,
+        }
+    }
+}
+
+impl ops::Mul<usize> for TimeVal {
+    type Output = TimeVal;
+
+    fn mul(self, rhs: usize) -> TimeVal {
+        let mut sec = self.sec * rhs;
+        let mut usec = self.usec * rhs;
+        if usec >= MICRO_PER_SEC {
+            sec += usec / MICRO_PER_SEC;
+            usec %= MICRO_PER_SEC;
+        }
+        TimeVal { sec, usec }
+    }
+}
+
+impl ops::Div<usize> for TimeVal {
+    type Output = TimeVal;
+
+    fn div(self, rhs: usize) -> TimeVal {
+        let mut sec = self.sec / rhs;
+        let mut usec = self.usec / rhs;
+        if self.usec % rhs != 0 {
+            let remainder = self.usec % rhs;
+            sec += remainder * MICRO_PER_SEC / rhs;
+            usec = (usec * MICRO_PER_SEC + remainder) / rhs;
+        }
+        TimeVal { sec, usec }
+    }
+}
+
+impl PartialOrd for TimeVal {
+    fn partial_cmp(&self, other: &TimeVal) -> Option<core::cmp::Ordering> {
+        if self.sec < other.sec {
+            Some(core::cmp::Ordering::Less)
+        } else if self.sec > other.sec {
+            Some(core::cmp::Ordering::Greater)
+        } else if self.usec < other.usec {
+            Some(core::cmp::Ordering::Less)
+        } else if self.usec > other.usec {
+            Some(core::cmp::Ordering::Greater)
+        } else {
+            Some(core::cmp::Ordering::Equal)
+        }
+    }
+}
+
+// region TimeVal end
+
+// region TimeUnit begin
+#[allow(unused)]
+pub enum TimeUnit {
+    Hour,
+    Min,
+    Sec,
+    Msec,
+    Usec,
+}
+// region TimeUnit end
