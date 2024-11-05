4710bc4caddc959033952136ff75c7869a06355b
Yttehs <shetty.yttehs@outlook.com>
2024-11-05 17:48:49 +0800

    chore: update test_sbrk

--- user/src/bin/05_test_sbrk.rs
+++ user/src/bin/05_test_sbrk.rs
@@ -9,10 +9,22 @@ extern crate user_lib;
 #[no_mangle]
 fn main() -> i32 {
     println!("[User] test_sbrk");
-    let ptr = sbrk(4096);
-    println!("[User] test_sbrk: sbrk(4096) = {:#x}", ptr as usize);
-    let ptr = sbrk(0);
-    println!("[User] test_sbrk: sbrk(0) = {:#x}", ptr as usize);
+    let old_ptr = sbrk(4096) as *mut u8;
+    println!("[User] test_sbrk: sbrk(4096) = {:#x}", old_ptr as usize);
+    let new_ptr = sbrk(0) as *mut u8;
+    println!("[User] test_sbrk: sbrk(0) = {:#x}", new_ptr as usize);
+
+    println!("[User] test_sbrk: heap size = {}", new_ptr as usize - old_ptr as usize);
+
+    println!("[User] test_sbrk: write to old_ptr");
+    unsafe {
+        *old_ptr = 114;
+    }
+
+    println!("[User] test_sbrk: read from old_ptr");
+    let value = unsafe { *old_ptr };
+    println!("[User] test_sbrk: *old_ptr = {}", value);
+
     println!("[User] test_sbrk: done");
     0
 }
