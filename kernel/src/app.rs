183abeb847842c7dcf1e6420efb4dac8baad2645
Yttehs <shetty.yttehs@outlook.com>
2024-11-08 18:13:34 +0800

    refactor: app

--- kernel/src/app.rs
+++ kernel/src/app.rs
@@ -2,21 +2,6 @@ use lazy_static::lazy_static;
 
 // Before implementing file system, we use include_bytes! to load the binary of the app
 const APP_NUM: usize = 7;
-const APP_MAX_SIZE: usize = 0x40000;
-const TEST_PRINT: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/01_test_print");
-const TEST_SRET: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/02_test_sret");
-const TEST_PAGE_FAULT: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/03_test_page_fault");
-const TEST_YIELD: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/04_test_yield");
-const TEST_SBRK: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/05_test_sbrk");
-const TEST_READ: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/06_test_read");
-const USER_SHELL: &[u8] =
-    include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/user_shell");
 
 pub fn get_app(name: &str) -> Option<&App> {
     APPS.iter().find(|app| app.name() == name)
@@ -24,37 +9,57 @@ pub fn get_app(name: &str) -> Option<&App> {
 
 lazy_static! {
     static ref APPS: [App; APP_NUM] = [
-        App::new(0, "test_print", TEST_PRINT),
-        App::new(1, "test_sret", TEST_SRET),
-        App::new(2, "test_page_fault", TEST_PAGE_FAULT),
-        App::new(3, "test_yield", TEST_YIELD),
-        App::new(4, "test_sbrk", TEST_SBRK),
-        App::new(5, "test_read", TEST_READ),
-        App::new(6, "user_shell", USER_SHELL),
+        App::new(
+            0,
+            "test_print",
+            include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/01_test_print")
+        ),
+        App::new(
+            1,
+            "test_sret",
+            include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/02_test_sret")
+        ),
+        App::new(
+            2,
+            "test_page_fault",
+            include_bytes!(
+                "../../user/target/riscv64gc-unknown-none-elf/release/03_test_page_fault"
+            )
+        ),
+        App::new(
+            3,
+            "test_yield",
+            include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/04_test_yield")
+        ),
+        App::new(
+            4,
+            "test_sbrk",
+            include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/05_test_sbrk")
+        ),
+        App::new(
+            5,
+            "test_read",
+            include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/06_test_read")
+        ),
+        App::new(
+            6,
+            "user_shell",
+            include_bytes!("../../user/target/riscv64gc-unknown-none-elf/release/user_shell")
+        ),
     ];
 }
 
-static mut ALIGNED_SPACES: [AlignedSpace; APP_NUM] = [AlignedSpace {
-    data: [0; APP_MAX_SIZE],
-}; APP_NUM];
-
 // region App begin
+#[repr(align(4096))]
 pub struct App {
     id: usize,
     name: &'static str,
-    space: &'static AlignedSpace,
+    elf: &'static [u8],
 }
 
 impl App {
     pub fn new(id: usize, name: &'static str, elf: &'static [u8]) -> Self {
-        unsafe {
-            ALIGNED_SPACES[id].copy_data(elf);
-            Self {
-                id,
-                name,
-                space: &ALIGNED_SPACES[id],
-            }
-        }
+        App { id, name, elf }
     }
 
     pub fn id(&self) -> usize {
@@ -64,25 +69,7 @@ impl App {
         self.name
     }
     pub fn elf(&self) -> &'static [u8] {
-        self.space.get_data()
+        self.elf
     }
 }
 // region App end
-
-// region AlignSpace begin
-#[repr(align(4096))]
-#[derive(Clone, Copy)]
-struct AlignedSpace {
-    data: [u8; APP_MAX_SIZE],
-}
-
-impl AlignedSpace {
-    pub fn copy_data(&mut self, data: &[u8]) {
-        self.data[..data.len()].copy_from_slice(data);
-    }
-
-    pub fn get_data(&self) -> &[u8] {
-        &self.data
-    }
-}
-// region AlignSpace end
