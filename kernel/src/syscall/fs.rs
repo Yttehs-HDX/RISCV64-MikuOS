47d4ea4ba41c4ac316166782a7d9428b969cf4d3
Yttehs <shetty.yttehs@outlook.com>
2024-11-09 22:37:34 +0800

    refactor: syscall write

--- kernel/src/syscall/fs.rs
+++ kernel/src/syscall/fs.rs
@@ -26,16 +26,14 @@ pub fn sys_read(fd: usize, buffer: *const u8, len: usize) -> isize {
 pub fn sys_write(fd: usize, buffer: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
-            // let slice = unsafe { core::slice::from_raw_parts(buffer, len) };
-            let slices = mm::translate_bype_buffer(
+            let buffers = mm::translate_bype_buffer(
                 task::get_processor().current().inner_mut().get_satp(),
                 buffer,
                 len,
-            );
-            for slice in slices {
-                let str = core::str::from_utf8(slice).unwrap();
-                print!("{}", str);
-            }
+            )
+            .concat();
+            let str = core::str::from_utf8(buffers.as_slice()).unwrap();
+            print!("{}", str);
             len as isize
         }
         _ => {
