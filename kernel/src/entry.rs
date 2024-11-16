9654a6805c2fa928f43730c32b0d8e7eaf0a57d1
Yttehs <shetty.yttehs@outlook.com>
2024-11-16 13:06:48 +0800

    chore: simplify entry code

--- kernel/src/entry.rs
+++ kernel/src/entry.rs
@@ -1,26 +1,33 @@
 use core::arch::asm;
 use crate::mm::{PTEFlags, PageTableEntry, PhysAddr};
 
+const SBI_ADDR: usize = 0x80000000;
+const BASE_ADDR: usize = 0xffffffffc0000000;
+const OFFSET: usize = BASE_ADDR - SBI_ADDR;
+
 #[naked]
 #[no_mangle]
 #[link_section = ".text.entry"]
 unsafe extern "C" fn _start() -> ! {
     asm!(
         // set sp
-        "lui sp, %hi(BOOT_STACK)",
+        "la sp, {boot_stack}",
+        "li t0, {offset}",
+        "add sp, sp, t0",
         // construct satp
-        "lui t0, %hi(ROOT_PAGE)",
-        "li t1, 0xffffffffc0000000 - 0x80000000",
-        "sub t0, t0, t1",
-        "srli t0, t0, 12",
-        "li t1, 8 << 60",
-        "or t0, t0, t1",
-        "csrw satp, t0",
+        "la t1, {root_page}",
+        "srli t1, t1, 12",
+        "li t2, 8 << 60",
+        "or t1, t1, t2",
+        "csrw satp, t1",
         "sfence.vma",
         // call rust_main
-        "lui t0, %hi(rust_main)",
-        "addi t0, t0, %lo(rust_main)",
-        "jr t0",
+        "la t1, rust_main",
+        "add t1, t1, t0",
+        "jr t1",
+        offset = const OFFSET,
+        boot_stack = sym BOOT_STACK,
+        root_page = sym ROOT_PAGE,
         options(noreturn)
     )
 }
@@ -34,7 +41,6 @@ static BOOT_STACK: [u8; 4096] = [0; 4096];
 static ROOT_PAGE: [PageTableEntry; 512] = {
     let flags = PTEFlags::from_bits_truncate(0xcf);
     let mut page = [PageTableEntry::empty(); 512];
-    const SBI_ADDR: usize = 0x80000000;
     page[2] = PageTableEntry::new(PhysAddr(SBI_ADDR).to_ppn(), flags);
     page[511] = PageTableEntry::new(PhysAddr(SBI_ADDR).to_ppn(), flags);
     page
