b794c0569ab46e8997ccc7e499e13ea590de0c8c
Yttehs <shetty.yttehs@outlook.com>
2024-12-03 20:38:20 +0800

    fix: OpenFlags fields

--- kernel/src/fs/interface/inode.rs
+++ kernel/src/fs/interface/inode.rs
@@ -34,21 +34,21 @@ pub enum InodeType {
 // region OpenFlags begin
 bitflags! {
     pub struct OpenFlags: u32 {
-        const RDONLY = 0;
-        const WRONLY = 1 << 0;
-        const RDWR = 1 << 1;
-        const CREATE = 1 << 9;
-        const TRUNC = 1 << 10;
+        const RDONLY = 0; // 0x0
+        const WRONLY = 1; // 0x1
+        const RDWR = 1 << 1; // 0x2
+        const CREATE = 1 << 6; // 0x40
+        const TRUNC = 1 << 10; // 0x400
+        const DIRECTORY = 1 << 21; // 0x200000
     }
 }
 
 impl OpenFlags {
     pub const fn read_write(&self) -> (bool, bool) {
         match self {
-            _ if self.contains(Self::RDONLY) => (true, false),
-            _ if self.contains(Self::WRONLY) => (false, true),
             _ if self.contains(Self::RDWR) => (true, true),
-            _ => (false, false),
+            _ if self.contains(Self::WRONLY) => (false, true),
+            _ => (true, false),
         }
     }
 
